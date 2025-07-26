f46cbe9947fa5d3c6fc37e805de4ae8c23d97cff
Rudranil Bhattacharya <rudranilms7@gmail.com>
2025-07-26 21:37:12 +0530

    Clean: remove duplicate root-level backend files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rudranilbhattacharya/demo-stock-trading-app/backend/venv"
+set -gx VIRTUAL_ENV "/Users/rudranilbhattacharya/Documents/demo-stock-trading-app/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
