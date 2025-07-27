17aaa2009070de3081c726a9bde4f7ad54395a12
Rudranil Bhattacharya <100760323+rudraa2005@users.noreply.github.com>
2025-07-27 14:48:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 import os
-
+import json
 from backend.auth import auth_bp
 from backend.dashboard import dashboard_bp
 from backend.portfolio import portfolio_bp
