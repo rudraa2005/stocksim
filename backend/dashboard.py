7d271b13d077637894951e521ca1d49e655e9115
Rudranil Bhattacharya <100760323+rudraa2005@users.noreply.github.com>
2025-07-27 11:59:29 +0530

    Update dashboard.py

--- backend/dashboard.py
+++ backend/dashboard.py
@@ -9,12 +9,8 @@ from urllib.request import urlopen
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase only once
-if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccount.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
+def get_db():
+    return firestore.client()
 cached_stocks = None
 
 @dashboard_bp.route("/dashboard")
