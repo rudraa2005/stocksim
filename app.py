506f48ea58bf63e941bb7767206ef15645f5f3ae
Rudranil Bhattacharya <100760323+rudraa2005@users.noreply.github.com>
2025-07-27 13:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from backend.trade import trade_bp
 
 app = Flask(__name__)
 CORS(app)
+firebase_config_str = os.environ.get("GOOGLE_CREDENTIALS")
+firebase_config = json.loads(firebase_config_str)
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred)
+
 
 app.secret_key = "your-secret-key"
 app.config["JWT_SECRET_KEY"] = "your-jwt-secret-key"
